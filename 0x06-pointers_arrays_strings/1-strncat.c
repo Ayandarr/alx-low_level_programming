845e2d00f993118de38cd94db5a8f1a7cb00e775
Ayandarr <ayandar2002@gmail.com>
2022-06-16 07:39:18 -0700

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,33 +1,15 @@
-#include "holberton.h"
+#include "main.h"
+#include <string.h>
 
 /**
- * _strncat - two words
- * @dest : pointer to char param
- * @src : pointer to char param
- * @n : int parameter
- * Return: *dest
- */
-
+* _strncat -> function to append some charx
+* @dest: first param
+* @src: second param
+* @n: 3rd param
+* Return: string
+*/
 char *_strncat(char *dest, char *src, int n)
 {
-	int m;
-	int i;
-
-	m = 0;
-
-	for (i = 0; i < 1000; i++)
-	{
-		if (dest[i] == '\0')
-		{
-			break;
-		}
-		m++;
-	}
-
-	for (i = 0; src[i] != '\0' && i < n; i++)
-	{
-		dest[m + i] = src[i];
-	}
-	dest[m + i] = '\0';
+	strncat(dest, src, n);
 	return (dest);
 }
